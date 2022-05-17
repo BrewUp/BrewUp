46bd7b37aba5742ca57282e8ef2adae4c6725e87
Alberto Acerbis <alberto.acerbis@intre.it>
2022-05-17 08:04:38 +0200

    Micro-Frontends
    - Try to share sessions

---
+++ NotEveryoneDrinksBlonde/Blazor/BrewUpWasm.Production/BrewUpWasm.Production/Client/Features/Index.razor.cs
@@ -0,0 +1,37 @@
+﻿using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components;
+
+namespace BrewUpWasm.Production.Client.Features;
+
+public class IndexBase : ComponentBase, IDisposable
+{
+    [Inject] private ISessionStorageService _sessionStorageService { get; set; }
+
+    protected string HelloMessage = "Nothing to Tell :-(";
+
+    protected override async Task OnInitializedAsync()
+    {
+        HelloMessage = await _sessionStorageService.GetItemAsync<string>("HelloMessage");
+
+        await base.OnInitializedAsync();
+    }
+
+    #region Dispose
+    public void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+        }
+    }
+    public void Dispose()
+    {
+        this.Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    ~IndexBase()
+    {
+        this.Dispose(false);
+    }
+    #endregion
+}
---
+++ NotEveryoneDrinksBlonde/Blazor/BrewUpWasm/BrewUpWasm/Client/Features/Index.razor.cs
@@ -0,0 +1,35 @@
+﻿using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components;
+
+namespace BrewUpWasm.Client.Features;
+
+public class IndexBase : ComponentBase, IDisposable
+{
+    [Inject] private ISessionStorageService _sessionStorageService { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await _sessionStorageService.SetItemAsync("HelloMessage", "Hello By General Module");
+
+        await base.OnInitializedAsync();
+    }
+
+    #region Dispose
+    public void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+        }
+    }
+    public void Dispose()
+    {
+        this.Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    ~IndexBase()
+    {
+        this.Dispose(false);
+    }
+    #endregion
+}
